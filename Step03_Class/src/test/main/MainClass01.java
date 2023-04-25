227093d96a5b1e91a87195194d967dd93574eaee
Jojungeun <title.jjk@gmail.com>
2023-04-25 18:31:34 +0900

    java study 20230425

---
+++ Step03_Class/src/test/main/MainClass01.java
@@ -0,0 +1,17 @@
+package test.main;
+
+import test.mypac.Car;
+
+public class MainClass01 {
+	// run 했을때 실행의 흐름이 시작되는 특별한 main 메소드
+	public static void main(String[] args) {
+		
+		//Car car = null;//패키지가 달라서 오류가 발생함 오류를 발생 안시키려면 Car라는 class를 import 해서 연결시켜줘야함.
+		Car car1 = new Car(); //class로 객체를 만드는 방법
+		Car car2 = new Car(); 
+		int num = 10;
+		
+		String name = "김구라";
+		name.length();
+	}
+}
